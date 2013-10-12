3b2264f786f7089f5d9fd6847820482f2e740e21
kojoman <kojo@happyfuncorp.com>
2013-10-12 09:47:57 -0400

    Exercise 1: A Good First Program.

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Prinint.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "I \"said\" do not touch this."
+#print "This is a comment."
