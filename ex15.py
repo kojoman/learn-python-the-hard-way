a3864f9c80329ac9ce8b63fbf1e0b97c1d53c85f
kojoman <kojo@happyfuncorp.com>
2013-10-16 23:41:25 -0400

    Read from CSV file

--- ex15.py
+++ ex15.py
@@ -1,4 +1,5 @@
 from sys import argv
+import csv
 
 script, filename = argv
 
@@ -7,9 +8,15 @@ txt = open(filename)
 print "Here's your file %r:" % filename
 print txt.read()
 
-print "Type the filename again:"
-file_again = raw_input("> ")
+# print "Type the filename again:"
+# file_again = raw_input("> ")
 
-txt_again = open(file_again)
+# txt_again = open(file_again)
 
-print txt_again.read()
+# print txt_again.read()
+
+# CSV file manipulation
+with open('world_bank_data_catalog.csv', 'rb') as csvfile:
+	worldBankCatalog = csv.reader(csvfile, delimiter=',', quotechar='|')
+	for row in worldBankCatalog:
+		print ','.join(row)
