79806fdbc5ae7433e8673f3b9138f192bcec95e1
kojoman <juantuah@gmail.com>
2013-12-02 22:53:18 -0500

    Exercise 18: Names, Variables, Code, Functions

---
+++ ex18.py
@@ -0,0 +1,26 @@
+# this one is like your scripts with argv
+def print_two(*args):
+	arg1, arg2 = args
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+#Ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# This just takes one argument 
+def print_one(arg1):
+	print "arg1: %r" % arg1
+
+# This one takes no arguments 
+def print_none():
+	print "I got nothing.."
+
+# Create function, "checklist"
+def checklist():
+	print "Zed says to create a function called checklist"
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
+print_one("First!")
+print_none()
+checklist()
